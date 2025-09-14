0fb98969f7c4fac37ab7c6f0f3950b230f529a92
nicrandomlee <leejpnicholas@gmail.com>
2025-09-14 22:50:25 +0800

    Update Smart Score Generator

--- stock_ticker_scraper.py
+++ stock_ticker_scraper.py
@@ -4,6 +4,7 @@ import re
 import requests
 import time
 
+from datetime import datetime
 from bs4 import BeautifulSoup
 from lxml import etree
 from selenium import webdriver
@@ -23,7 +24,7 @@ def scrape_table(table):
         data.append(row_data)
     return data
 
-def get_ticker_smart_score(exch = "sp500", top_k = 20):
+def get_ticker_smart_score(exch = "sp500", top_k = 150):
     '''
     exch: One of ["sp500", "nyse"]
     '''
@@ -63,8 +64,25 @@ def get_ticker_smart_score(exch = "sp500", top_k = 20):
                 EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[2]/div[4]"))
             )
             smart_score = re.search(r"Stock Smart Score\s*(\d+)", element.text).group(1)
-            smart_score_results.append((stock_ticker, smart_score))
+            avg_price_tgt = re.search(r"Average Price Target:\s*\$(\d+\.\d+)", element.text).group(1)
+            smart_score_results.append((stock_ticker, smart_score, avg_price_tgt))
         except:
             print(f"Error printing smart score for {stock_ticker}")
             continue
+
+    res = df.iloc[:top_k]
+    smart_score_df = pd.DataFrame(smart_score_results, columns=["Symbol", "Smart Score", "Average Price Target"])
+    excel_output = pd.merge(smart_score_df, res)
+    excel_output.drop("No.", inplace=True, axis=1)
+    excel_output["Smart Score"] = excel_output["Smart Score"].astype(int)
+    excel_output["Average Price Target"] = excel_output["Average Price Target"].astype(float)
+    excel_output["Stock Price"] = excel_output["Stock Price"].astype(float)
+    excel_output["Upside"] = (excel_output["Average Price Target"] - excel_output["Stock Price"]) / excel_output["Stock Price"]
+    excel_output["Upside"] = (excel_output["Upside"] * 100).round(2).astype(str) + "%"
+    today_str = datetime.today().strftime('%Y%m%d')
+    excel_output.to_excel(f'scraped_scores/{today_str}_top_{top_k}_smart_score.xlsx')
+
     return sorted(smart_score_results, key=lambda x: int(x[1]), reverse=True)
+
+if __name__ == "__main__":
+    get_ticker_smart_score(exch="sp500", top_k=150)
